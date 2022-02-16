e580be29ae1e80bf2a76b2b261fc46ace448fb14
Mayuresh <mayuresh.deore02@gmail.com>
2022-02-16 18:49:35 +0530

    commit

--- product.js
+++ product.js
@@ -1,27 +1,3 @@
-$(".carouselEg").owlCarousel(
-  {
-    margin:20,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 2000,
-    autoplayHoverPause: true,
-    responsive:{
-      0:{
-        items:1,
-        nav: false
-      },
-      600:{
-        items:2,
-        nav: false
-      },
-      1000:{
-        items:3,
-        nav: false
-      }
-    }
-  }
-);
-// For Login In Page
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
